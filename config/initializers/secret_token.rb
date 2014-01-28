478ff5642b510df6ce85b83e13fa57e8d8b582b5
John Ciancanelli <jciancanelli61@att.net>
2014-01-27 23:00:47 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PainterPlus::Application.config.secret_token = '6332a52607041bdf9afca34d77d43e2523d6c1f3040123adc947e83739de1cb331cef1e03d26b072a144e4016e3f657e409cbb6115cf76306807b013024bc903'
---
+++ painterPlus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PainterPlus::Application.config.secret_token = '2dbc03036a882d72a42d00054191f5f88238c219060fb8ee5aea5709ff204bc6392efbbc29aaa9c6864542ff5d9fe1f790ff24e655fd5ed0c3181c9e9ed8e2f1'
